0460ec2bb18a61cd1c6751ac689c61f4441cda28
Naka <ckaksj97@gmail.com>
2021-08-31 13:47:42 +0900

    던전 구현 및 Mapper 사용

--- src/main/rpg/character/character.java
+++ src/main/rpg/character/character.java
@@ -1,15 +1,16 @@
 package main.rpg.character;
 
 public class Character {
-
+	private int AccountId;
 	private String name;
 	private int level;
 	private double exp;
 	private int dungeonLevel;
 
 
-	public Character(String name, int level, double exp, int dungeonLevel) {
+	public Character(int Accountid, String name, int level, double exp, int dungeonLevel) {
 		super();
+		this.AccountId = Accountid;
 		this.name = name;
 		this.level = level;
 		this.exp = exp;
@@ -19,6 +20,10 @@ public class Character {
 	@Override
 	public String toString() {
 		return "캐릭터 정보 [이름= " + name + ", 레벨= " + level + ", 경험치= " + exp + "]";
+	}
+
+	public int getAccountId() {
+		return AccountId;
 	}
 
 	public String getName() {
